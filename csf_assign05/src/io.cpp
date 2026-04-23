8b454acb7052c84041b9d02f5edfa237ecdd631c
Annabelle Li-Kroeger <alikroe1@jh.edu>
2026-04-22 23:02:19 -0400

    finished io

--- csf_assign05/src/io.cpp
+++ csf_assign05/src/io.cpp
@@ -7,7 +7,33 @@
 
 namespace {
 
-// TODO: define helper functions
+  // Helper function: put bytes from fd into message
+  void put_to_buffer(int fd, char *buf, size_t byte_size) {
+    size_t i = 0;
+    // read bytes from file
+    while (i < byte_size) {
+      ssize_t my_byte = read(fd, buf + i, byte_size - i);
+      // read in an end of file character
+      if (my_byte == 0) {
+        throw IOException("file ended early");
+      }
+      // handle invalid character
+      if (my_byte < 0) {
+        throw IOException("invalid character, read error");
+      }
+      i += my_byte;
+    }
+  }
+
+  // Helper function: ensures that string message is valid
+  bool ensure_valid_string(char *message) {
+    // loop through string and make sure that all values are valid
+    for (int i = 0; i < 4; i++) {
+      if (message[i] < '0' || message[i] > '9')
+        return false;
+    }
+    return true;
+  }
 
 
 } // end of anonymous namespace for helper functions
@@ -30,7 +56,30 @@ void send(const std::string &s, int outfd) {
 }
 
 void receive(int infd, std::string &s) {
-  // TODO: implement
+  // put infd into new char array
+  char message[5] = {0};
+  put_to_buffer(infd, message, 4);
+
+  // ensure that put_to_buffer created valid message
+  if (!ensure_valid_string(message)) {
+    throw IOException("string is not valid");
+  }
+
+  // ensure that message length is valid
+  int my_length = std::stoi(std::string(message, 4));
+  if (my_length < 1) {
+    throw IOException("string length is not valid");
+  }
+
+  std::string message_1(my_length, '\0');
+  put_to_buffer(infd, &message_1[0], my_length);
+ 
+  // Last byte must be newline
+  if (message_1.back() != '\n')
+    throw IOException("missing terminating newline");
+ 
+  // Strip the newline
+  s = message_1.substr(0, my_length - 1);
 }
 
 } // end of IO namespace
