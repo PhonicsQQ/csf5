7c2479f0f8966dafa3914db74eec804e5dc227f5
Annabelle Li-Kroeger <alikroe1@jh.edu>
2026-04-22 21:02:34 -0400

    added send function

--- csf_assign05/src/io.cpp
+++ csf_assign05/src/io.cpp
@@ -15,7 +15,18 @@ namespace {
 namespace IO {
 
 void send(const std::string &s, int outfd) {
-  // TODO: implement
+  // must frame s with an end character
+  size_t len = s.size() + 1;
+
+  //ensure that length output is 4 digits with leading zeros
+  std::ostringstream oss;
+  oss << std::setw(4) << std::setfill('0') << len;
+  std::string my_frame = oss.str() + s + "\n";
+
+  //check if bytes were written correctly
+  if (rio_writen(outfd, my_frame.data(), my_frame.size()) != ssize_t(my_frame.size())) {
+    throw IOException("written frame size does not match expected frame size");
+  }
 }
 
 void receive(int infd, std::string &s) {
